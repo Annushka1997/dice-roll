d6dc8f3e2a1fc9d7c0ee4aacf923ec0bbf0a2ad7
Annushka1997 <annushkach1997@mail.ru>
2023-06-01 13:05:31 +0400

    Optimization

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 "use strict";
 
 // Variables 
-
 const statusBtn = document.querySelector(".status");
 const startBtn = document.querySelector(".startBtn");
 const diceCircles1 = document.querySelectorAll("[data-dice1]");
@@ -18,139 +17,52 @@ startBtn.addEventListener("click", () => {
     let randomNumber1 = Math.floor(Math.random() * (7 - 1) + 1);
     let randomNumber2 = Math.floor(Math.random() * (7 - 1) + 1); 
 
-    diceRoll();
+    diceRoll(dice[0],diceCircles1);
+    diceRoll(dice[1],diceCircles2);
+
     setTimeout(() => {
         dice[0].classList.remove("active");
         dice[1].classList.remove("active");
-        checkdiceRoll1(randomNumber1);
-        checkdiceRoll2(randomNumber2);
+        checkdiceRoll(randomNumber1,diceCircles1);
+        checkdiceRoll(randomNumber2,diceCircles2);
         checkWin(randomNumber1,randomNumber2);
     },1500);
 });
 
 // Functions
-
-function diceRoll () {
-        rollAudio.play();
-        dice[0].classList.add("active");
-        dice[1].classList.add("active");
-        diceCircles1[0].classList.add("active");
-        diceCircles1[1].classList.add("active");
-        diceCircles1[2].classList.add("active");
-        diceCircles1[3].classList.remove("active");
-        diceCircles1[4].classList.add("active");
-        diceCircles1[5].classList.add("active");
-        diceCircles1[6].classList.add("active");
-        diceCircles2[0].classList.add("active");
-        diceCircles2[1].classList.add("active");
-        diceCircles2[2].classList.add("active");
-        diceCircles2[3].classList.remove("active");
-        diceCircles2[4].classList.add("active");
-        diceCircles2[5].classList.add("active");
-        diceCircles2[6].classList.add("active");
-}
-
-function checkdiceRoll1 (dice) {
-    if (dice === 1) {
-        diceCircles1[0].classList.remove("active");
-        diceCircles1[1].classList.remove("active");
-        diceCircles1[2].classList.remove("active");
-        diceCircles1[3].classList.add("active");
-        diceCircles1[4].classList.remove("active");
-        diceCircles1[5].classList.remove("active");
-        diceCircles1[6].classList.remove("active");
-    } else if (dice === 2) {
-        diceCircles1[0].classList.add("active");
-        diceCircles1[1].classList.remove("active");
-        diceCircles1[2].classList.remove("active");
-        diceCircles1[3].classList.remove("active");
-        diceCircles1[4].classList.remove("active");
-        diceCircles1[5].classList.remove("active");
-        diceCircles1[6].classList.add("active");
-    } else if (dice === 3) {
-        diceCircles1[0].classList.add("active");
-        diceCircles1[1].classList.remove("active");
-        diceCircles1[2].classList.remove("active");
-        diceCircles1[3].classList.add("active");
-        diceCircles1[4].classList.remove("active");
-        diceCircles1[5].classList.remove("active");
-        diceCircles1[6].classList.add("active");
-    } else if (dice === 4) {
-        diceCircles1[0].classList.add("active");
-        diceCircles1[1].classList.remove("active");
-        diceCircles1[2].classList.add("active");
-        diceCircles1[3].classList.remove("active");
-        diceCircles1[4].classList.add("active");
-        diceCircles1[5].classList.remove("active");
-        diceCircles1[6].classList.add("active");
-    } else if (dice === 5) {
-        diceCircles1[0].classList.add("active");
-        diceCircles1[1].classList.remove("active");
-        diceCircles1[2].classList.add("active");
-        diceCircles1[3].classList.add("active");
-        diceCircles1[4].classList.add("active");
-        diceCircles1[5].classList.remove("active");
-        diceCircles1[6].classList.add("active");
-    } else {
-        diceCircles1[0].classList.add("active");
-        diceCircles1[1].classList.add("active");
-        diceCircles1[2].classList.add("active");
-        diceCircles1[3].classList.remove("active");
-        diceCircles1[4].classList.add("active");
-        diceCircles1[5].classList.add("active");
-        diceCircles1[6].classList.add("active");
+function diceRoll (dice,diceCircle) {
+    rollAudio.play();
+    dice.classList.add("active");
+    for (let i = 0; i < 7; i++) {
+        i === 3 ? diceCircle[i].classList.remove("active") : diceCircle[i].classList.add("active");
     }
 }
 
-function checkdiceRoll2 (dice) {
+function checkdiceRoll (dice,diceCircle) {
     if (dice === 1) {
-        diceCircles2[0].classList.remove("active");
-        diceCircles2[1].classList.remove("active");
-        diceCircles2[2].classList.remove("active");
-        diceCircles2[3].classList.add("active");
-        diceCircles2[4].classList.remove("active");
-        diceCircles2[5].classList.remove("active");
-        diceCircles2[6].classList.remove("active");
+        for (let i = 0; i < 7; i++) {
+            i === 3 ? diceCircle[i].classList.add("active") : diceCircle[i].classList.remove("active");
+        }
     } else if (dice === 2) {
-        diceCircles2[0].classList.add("active");
-        diceCircles2[1].classList.remove("active");
-        diceCircles2[2].classList.remove("active");
-        diceCircles2[3].classList.remove("active");
-        diceCircles2[4].classList.remove("active");
-        diceCircles2[5].classList.remove("active");
-        diceCircles2[6].classList.add("active");
+        for (let i = 0; i < 7; i++) {
+            i === 0 || i === 6 ? diceCircle[i].classList.add("active") : diceCircle[i].classList.remove("active");
+        }
     } else if (dice === 3) {
-        diceCircles2[0].classList.add("active");
-        diceCircles2[1].classList.remove("active");
-        diceCircles2[2].classList.remove("active");
-        diceCircles2[3].classList.add("active");
-        diceCircles2[4].classList.remove("active");
-        diceCircles2[5].classList.remove("active");
-        diceCircles2[6].classList.add("active");
+        for (let i = 0; i < 7; i++) {
+            i === 0 || i === 3 || i === 6 ? diceCircle[i].classList.add("active") : diceCircle[i].classList.remove("active");
+        }
     } else if (dice === 4) {
-        diceCircles2[0].classList.add("active");
-        diceCircles2[1].classList.remove("active");
-        diceCircles2[2].classList.add("active");
-        diceCircles2[3].classList.remove("active");
-        diceCircles2[4].classList.add("active");
-        diceCircles2[5].classList.remove("active");
-        diceCircles2[6].classList.add("active");
+        for (let i = 0; i < 7; i++) {
+            i === 1 || i === 3 || i === 5 ? diceCircle[i].classList.remove("active") : diceCircle[i].classList.add("active");
+        }
     } else if (dice === 5) {
-        diceCircles2[0].classList.add("active");
-        diceCircles2[1].classList.remove("active");
-        diceCircles2[2].classList.add("active");
-        diceCircles2[3].classList.add("active");
-        diceCircles2[4].classList.add("active");
-        diceCircles2[5].classList.remove("active");
-        diceCircles2[6].classList.add("active");
+        for (let i = 0; i < 7; i++) {
+            i === 1 || i === 5 ? diceCircle[i].classList.remove("active") :  diceCircle[i].classList.add("active");
+        }
     } else {
-        diceCircles2[0].classList.add("active");
-        diceCircles2[1].classList.add("active");
-        diceCircles2[2].classList.add("active");
-        diceCircles2[3].classList.remove("active");
-        diceCircles2[4].classList.add("active");
-        diceCircles2[5].classList.add("active");
-        diceCircles2[6].classList.add("active");
+        for (let i = 0; i < 7; i++) {
+            i === 3 ? diceCircle[i].classList.remove("active") : diceCircle[i].classList.add("active");
+        }
     }
 }
 
